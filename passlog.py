ed10b9cf05204975b19ec9e81fc5d217b8dd55f9
slipcasterme <vffjvndt@gmail.com>
2024-11-01 02:02:45 +1000

    create GUI without logic

--- passlog.py
+++ passlog.py
@@ -1,3 +1,33 @@
+from tkinter import *
+from tkinter import messagebox
+
+window = Tk()
+window.title('Passlog v1')
+window.geometry('1080x540')
+frame = Frame(
+    window,
+    padx = 10,
+    pady = 10,
+)
+frame.pack(expand = True)
+passlog_lb = Label(
+    frame,
+    text='Введите логин и пароль через запятую'
+)
+passlog_lb.grid(row=3, column=1)
+passlog_tf = Entry(
+    frame,
+)
+passlog_tf.grid(row=3, column = 2)
+reg_btn = Button(
+    frame,
+    text='Зарегистрироваться'
+)
+reg_btn.grid(row = 4, column = 2)
+
+
+window.mainloop()
+
 def check_one(passandlog):    #программа для создания пароля
     if len(passandlog) == 0:
         print('Вы ничего не ввели')
